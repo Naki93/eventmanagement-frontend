ed9afb342d3ea8958fa556751d18abe13f940f28
Anna Mashala <annamashala@gmail.com>
2023-08-21 02:47:15 +0200

    initial commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import splash from "../images/transparent.png"
+import './HomePage.css';
+
+
+//Homepage component displaying company name and login / signin buttons
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      {/* Navigation Bar */}
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+        
+          <a className="navbar-brand logo" href="#">Eventify</a>
+          <span className="">Your Go-To Event Platform</span>
+          <div className="login-signup">
+            <a href="/login" className="btn btn-primary ">Login</a>
+            <a href="/register" className="btn btn-warning ">Sign Up</a>
+          </div>
+        </div>
+      </nav>
+
+      {/* Content */}
+      
+      <div className="about ">
+        <div className="about-content">
+          {/* Left Side */}
+          <div className="text-content">
+            <div className="left-side">
+              <h1>Eventify is your go-to platform for managing and discovering upcoming events.</h1>
+              <p>Whether you're a conference center looking to showcase your events or an attendee eager to explore exciting gatherings, Eventify has you covered.</p>
+              <p>
+                <strong>Effortless Event Registration Seamless Event Management Enhanced Event Visibility</strong>
+              </p>
+            </div>
+          </div>
+
+          <div className=" ">
+            <div className="right-side">
+              <img src={splash} width ="600px" className='profile-image' alt="Event" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
+
+export default HomePage;
+
+
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  // Other webpack configuration options...
+
+  plugins: [
+    new Dotenv(),
+  ],
+};
